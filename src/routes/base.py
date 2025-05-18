a0f53f4add304844ec33545550f3e95bd73c95c5
AbdelrahmanMohamed11 <pintorcoating@gmail.com>
2025-05-18 03:45:47 +0300

    small update on base

--- src/routes/base.py
+++ src/routes/base.py
@@ -8,6 +8,6 @@ base_router = APIRouter(prefix="/base",
 @base_router.get("/")
 async def read_root():
     app_settings = get_settings()
-    app_name = app_settings.APP_NAME.value
-    app_version = app_settings.APP_VERSION.value
+    app_name = app_settings.APP_NAME
+    app_version = app_settings.APP_VERSION
     return {"app_name": app_name, "app_version": app_version}
